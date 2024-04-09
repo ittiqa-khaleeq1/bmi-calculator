1fcae47393eb594048ae630483d5b3bedae933fd
ittiqa-khaleeq1 <ittiqakhan1@gmail.com>
2024-04-09 18:26:23 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let weightInKg = await inquirer.prompt([
+	{
+		name: "weight",
+		type: "number",
+		message: chalk.magentaBright("Enter your weight in Kg's: ")
+	}
+]);
+
+let heightInMeters = await inquirer.prompt([
+	{
+		name: "height",
+		type: "number",
+		message: chalk.magentaBright("Enter your height in Meters: ")
+	}
+]);
+
+const bmi = weightInKg.weight / (heightInMeters.height * heightInMeters.height);
+console.log(chalk.green("Your BMI is:"), chalk.yellow(`${bmi}`));
